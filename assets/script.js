09b55d49aee141ff06a6a7d6234ab806a6cd9648
Marcel <marcelfrizler@protonmail.com>
2022-12-16 10:56:57 +0100

    Bessere Lösungvalidierung

--- assets/script.js
+++ assets/script.js
@@ -36,11 +36,14 @@ function checkSolution(aufgabe) {
 }
 
 function pythagoras1(solutionbox) {
-    let eingabe = solutionbox.value;
-    if (eingabe == 101) {
+    setAnswerButton(solutionbox, solutionbox.value == 101);
+}
+
+function setAnswerButton(solutionbox, isRight) {
+    if (isRight) {
         solutionbox.style.border = 'solid 2px lawngreen';
         solutionbox.style.backgroundColor = 'rgba(0, 200, 0, 0.2)';
-    }else{
+    } else {
         solutionbox.style.border = 'solid 2px red';
         solutionbox.style.backgroundColor = 'rgba(200, 0, 0, 0.2)';
     }
